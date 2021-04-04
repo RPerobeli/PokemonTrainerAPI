7d32b6116d57945bf694610e55c4e5ddca6adef0
Rodrigo <rodrigo.costa2015@engenharia.ufjf.br>
2021-04-04 17:26:52 -0300

    feat(config): configurações do projeto

--- PokemonTrainerAPI/Startup.cs
+++ PokemonTrainerAPI/Startup.cs
@@ -2,11 +2,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PokemonTrainerAPI.Repository;
+using PokemonTrainerAPI.Repository.Interfaces;
+using PokemonTrainerAPI.Services;
+using PokemonTrainerAPI.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,10 +33,18 @@ namespace PokemonTrainerAPI
         {
 
             services.AddControllers();
+            services.AddDbContext<PkTrainerContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PokemonTrainerAPI", Version = "v1" });
             });
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPokemonRepository, PokemonRepository>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IPokemonService, PokemonService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,12 +53,6 @@ namespace PokemonTrainerAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PokemonTrainerAPI v1");
-                    //c.RoutePrefix = string.Empty;
-                });
             }
 
             app.UseHttpsRedirection();
@@ -57,6 +64,13 @@ namespace PokemonTrainerAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PokemonTrainerAPI v1");
+                //c.RoutePrefix = string.Empty;
             });
         }
     }
